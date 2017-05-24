c316b2b902b0f11ce036a54411b59a61b8349987
Helena Nevskaya <helena.nevskaya@ya.ru>
2017-05-24 16:40:15 +0300

    updated

--- structs.go
+++ structs.go
@@ -58,26 +58,26 @@ type Aggregate struct {
 }
 
 type Service struct {
-	Id                  string  `json:"id,omitempty"`            // unique id
-	Code                string  `json:"code,omitempty"`          // previous service id
-	Price               int     `json:"price,omitempty"`         // в целых рублях
-	RetryDays           int     `json:"retry_days,omitempty"`    // for retries - days to keep retries, for periodic - subscription is alive
-	InactiveDays        int     `json:"inactive_days,omitempty"` // days of unsuccessful charge turns subscription into inactive state
-	GraceDays           int     `json:"grace_days,omitempty"`    // days in end of subscription period where always must be charged OK
-	PaidHours           int     `json:"paid_hours,omitempty"`    // rejected rule
-	DelayHours          int     `json:"delay_hours,omitempty"`   // repeat charge delay
-	SMSOnSubscribe      string  `json:"sms_on_unsubscribe,omitempty"`
-	SMSOnCharged        string  `json:"sms_on_charged,omitempty"`
-	SMSOnUnsubscribe    string  `json:"sms_on_subscribe,omitempty"` // if empty, do not send
-	SMSOnContent        string  `json:"sms_on_content,omitempty"`
-	SMSOnRejected       string  `json:"sms_on_rejected,omitempty"`
-	SMSOnBlackListed    string  `json:"sms_on_blacklisted,omitempty"`
-	SMSOnPostPaid       string  `json:"sms_on_postpaid,omitempty"`
-	PeriodicAllowedFrom int     `json:"periodic_allowed_from,omitempty"` // send content in sms allowed from and to times.
-	PeriodicAllowedTo   int     `json:"periodic_allowed_to,omitempty"`
-	PeriodicDays        string  `json:"periodic_days,omitempty"` // days of week to charge subscriber
-	MinimalTouchTimes   int     `json:"minimal_touch_times,omitempty"`
-	ContentIds          []int64 `json:"content_ids,omitempty"`
+	Id                  string   `json:"id,omitempty"`            // unique id
+	Code                string   `json:"code,omitempty"`          // previous service id
+	Price               int      `json:"price,omitempty"`         // в целых рублях
+	RetryDays           int      `json:"retry_days,omitempty"`    // for retries - days to keep retries, for periodic - subscription is alive
+	InactiveDays        int      `json:"inactive_days,omitempty"` // days of unsuccessful charge turns subscription into inactive state
+	GraceDays           int      `json:"grace_days,omitempty"`    // days in end of subscription period where always must be charged OK
+	PaidHours           int      `json:"paid_hours,omitempty"`    // rejected rule
+	DelayHours          int      `json:"delay_hours,omitempty"`   // repeat charge delay
+	SMSOnSubscribe      string   `json:"sms_on_unsubscribe,omitempty"`
+	SMSOnCharged        string   `json:"sms_on_charged,omitempty"`
+	SMSOnUnsubscribe    string   `json:"sms_on_subscribe,omitempty"` // if empty, do not send
+	SMSOnContent        string   `json:"sms_on_content,omitempty"`
+	SMSOnRejected       string   `json:"sms_on_rejected,omitempty"`
+	SMSOnBlackListed    string   `json:"sms_on_blacklisted,omitempty"`
+	SMSOnPostPaid       string   `json:"sms_on_postpaid,omitempty"`
+	PeriodicAllowedFrom int      `json:"periodic_allowed_from,omitempty"` // send content in sms allowed from and to times.
+	PeriodicAllowedTo   int      `json:"periodic_allowed_to,omitempty"`
+	PeriodicDays        string   `json:"periodic_days,omitempty"` // days of week to charge subscriber
+	MinimalTouchTimes   int      `json:"minimal_touch_times,omitempty"`
+	ContentIds          []string `json:"content_codes,omitempty"`
 }
 
 type Campaign struct {
@@ -97,7 +97,7 @@ type Campaign struct {
 }
 
 type PixelSetting struct {
-	Id            int64
+	Id            string
 	CampaignCode  string
 	OperatorCode  int64
 	Publisher     string
